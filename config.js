083494b0895d5de6a7ecf8c321b639989bae1b11
Tyler <tyler-franklin@outlook.com>
2025-09-09 16:00:10 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,25 +1,21 @@
-// --- CONFIGURATION FILE ---
-// Easily edit your departments, their colors, and their units here.
-
 export const departmentUnits = {
-    'Springfield Fire Dept': {
-        color: 'bg-red-600',
-        hover: 'hover:bg-red-700',
-        units: ['Rescue 1', 'Medic 1', 'Hazmat 1', 'Rescue 1', 'Battalion 1']
+    "SPFLD TWP": {
+        abbreviation: "STFD",
+        color: "bg-red-600",
+        hover: "hover:bg-red-700",
+        units: ["ENGINE 58", "MEDIC 58", "MEDIC 58A", "LADDER 58", "BRUSH 58", "ATV 58", "ENGINE 72", "TANKER 72", "BRUSH 72", "MEDIC 72", "RESCUE 72", "MEDIC 56", "ENGINE 56", "TANKER 56"]
     },
-    'Springfield Township Fire': {
-        color: 'bg-blue-600',
-        hover: 'hover:bg-blue-700',
-        units: ['Engine 58', 'Medic 58', 'Medic 58A', 'Ladder 58', 'ATV 58', 'Chief 2-58']
+    "SPFLD FIRE": {
+        abbreviation: "SFD",
+        color: "bg-blue-600",
+        hover: "hover:bg-blue-700",
+        units: ["BATTALION 1", "RESCUE 1", "MEDIC 1", "HAZMAT 1", "ENGINE 4", "MEDIC 4", "ENGINE 5", "MEDIC 5", "ENGINE 6", "MEDIC 6", "TRUCK 7", "MEDIC 7", "TRUCK 8", "MEDIC 8"]
     },
-    'Mutual Aid': {
-        color: 'bg-yellow-600',
-        hover: 'hover:bg-yellow-700',
-        units: ['MA Engine 1', 'MA Ladder 1', 'MA Medic']
-    },
-    'Command Staff': {
-        color: 'bg-gray-500',
-        hover: 'hover:bg-gray-600',
-        units: ['Safety Officer', 'Command Aide', 'PIO']
+    "HARMONY FIRE": {
+        abbreviation: "HTFD",
+        color: "bg-green-600",
+        hover: "hover:bg-green-700",
+        units: ["ENGINE 76", "TANKER 76", "RESCUE 76", "MEDIC 1-76", "MEDIC 2-76", "BRUSH 76", "ENGINE 2-76", "SUPPORT 76"]
     }
 };
+
